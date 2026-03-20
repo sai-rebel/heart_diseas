d8d28571b50fda8e0ccc8827a54757a85f6a8c24
sai-rebel <malavolusaiganesh@example.com>
2026-03-20 09:37:34 +0530

    fixed templates path

--- Cardiovascular_health_prediction/settings.py
+++ Cardiovascular_health_prediction/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'Cardiovascular_health_prediction.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
